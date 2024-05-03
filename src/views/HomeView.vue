96520fac7873030fdbdb145f6db5d9932061e3fd
Mattia Magi <mattia.magi91@gmail.com>
2024-05-03 19:34:44 +0200

    purchase sync wip

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import MeshTest from '../components/MeshTest.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <MeshTest />
   </main>
 </template>
